52a3eeb89e2903d0853ec29768665c6bc87c969b
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-09-21 21:41:30 -0300

    Modificada para receber arquivo xml

--- src/main/java/br/com/lucianobrsts/pessoa/controller/CadastrarPessoaController.java
+++ src/main/java/br/com/lucianobrsts/pessoa/controller/CadastrarPessoaController.java
@@ -1,8 +1,20 @@
 package br.com.lucianobrsts.pessoa.controller;
 
+import java.io.IOException;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.primefaces.model.file.UploadedFile;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import br.com.lucianobrsts.model.PessoaModel;
 import br.com.lucianobrsts.repository.PessoaRepository;
@@ -15,35 +27,116 @@ public class CadastrarPessoaController {
 
 	@Inject
 	PessoaModel pessoaModel;
-
+ 
 	@Inject
 	UsuarioController usuarioController;
-
+ 
 	@Inject
 	PessoaRepository pessoaRepository;
-
+ 
+ 
+	private UploadedFile file;
+ 
+	public UploadedFile getFile() {
+		return file;
+	}
+ 
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+ 
 	public PessoaModel getPessoaModel() {
 		return pessoaModel;
 	}
-
+ 
 	public void setPessoaModel(PessoaModel pessoaModel) {
 		this.pessoaModel = pessoaModel;
 	}
-
+ 
 	/**
-	 * SALVA UM NOVO REGISTRO VIA INPUT
+	 *SALVA UM NOVO REGISTRO VIA INPUT 
 	 */
-
-	public void salvarNovaPessoa() {
+	public void SalvarNovaPessoa(){
+ 
 		pessoaModel.setUsuarioModel(this.usuarioController.GetUsuarioSession());
-
-		// INFORMANDO QUE O CADASTRO FOI VIA INPUT
+ 
+		//INFORMANDO QUE O CADASTRO FOI VIA INPUT
 		pessoaModel.setOrigemCadastro("I");
-
+ 
 		pessoaRepository.salvarNovoRegistro(this.pessoaModel);
-
+ 
 		this.pessoaModel = null;
-
+ 
 		Uteis.MensagemInfo("Registro cadastrado com sucesso");
+ 
 	}
+ 
+	/**
+	 * REALIZANDO UPLOAD DE ARQUIVO
+	 */
+	 public void UploadRegistros() {
+ 
+		 DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+ 
+		 try {
+ 
+ 
+			 if(this.file.getFileName().equals("")){
+				 Uteis.MensagemAtencao("Nenhum arquivo selecionado!");
+				 return;
+			 }
+ 
+			 DocumentBuilder builder = factory.newDocumentBuilder();
+ 
+	                 Document doc = builder.parse(this.file.getInputStream());
+ 
+	                 Element element = doc.getDocumentElement();
+ 
+	                 Node nodes = (Node) element.getChildNodes();
+ 
+	                for (int i = 0; i < ((NodeList) nodes).getLength(); i++) {
+ 
+	        	     Node node  = ((NodeList) nodes).item(i);
+ 
+	        	    if(node.getNodeType() == Node.ELEMENT_NODE){
+ 
+	        		 Element elementPessoa =(Element) node;
+ 
+	        		 //PEGANDO OS VALORES DO ARQUIVO XML
+	        		 String nome     = elementPessoa.getElementsByTagName("nome").item(0).getChildNodes().item(0).getNodeValue();
+	        		 String sexo     = elementPessoa.getElementsByTagName("sexo").item(0).getChildNodes().item(0).getNodeValue();
+	        		 String email    = elementPessoa.getElementsByTagName("email").item(0).getChildNodes().item(0).getNodeValue();
+	        		 String endereco = elementPessoa.getElementsByTagName("endereco").item(0).getChildNodes().item(0).getNodeValue();
+ 
+	        		 PessoaModel newPessoaModel = new PessoaModel();
+ 
+	        		 newPessoaModel.setUsuarioModel(this.usuarioController.GetUsuarioSession());
+	        		 newPessoaModel.setEmail(email);
+	        		 newPessoaModel.setEndereco(endereco);
+	        		 newPessoaModel.setNome(nome);
+	        		 newPessoaModel.setOrigemCadastro("X");
+	        		 newPessoaModel.setSexo(sexo);
+ 
+	        		 //SALVANDO UM REGISTRO QUE VEIO DO ARQUIVO XML
+	        		 pessoaRepository.salvarNovoRegistro(newPessoaModel);
+	        	   }
+	              }
+ 
+ 
+ 
+		     Uteis.MensagemInfo("Registros cadastrados com sucesso!");
+ 
+		} catch (ParserConfigurationException e) {
+ 
+			e.printStackTrace();
+ 
+		} catch (SAXException e) {
+ 
+			e.printStackTrace();
+ 
+		} catch (IOException e) {
+ 
+			e.printStackTrace();
+		}
+	 }
 }
