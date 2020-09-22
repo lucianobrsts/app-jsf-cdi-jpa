9d144d80905806578769b8450ceb97088d8f6141
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-09-21 22:27:32 -0300

    Criado metodo getPessoas para preencher o dataTable

--- src/main/java/br/com/lucianobrsts/repository/PessoaRepository.java
+++ src/main/java/br/com/lucianobrsts/repository/PessoaRepository.java
@@ -1,11 +1,16 @@
 package br.com.lucianobrsts.repository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import br.com.lucianobrsts.model.PessoaModel;
+import br.com.lucianobrsts.model.UsuarioModel;
 import br.com.lucianobrsts.repository.entity.PessoaEntity;
 import br.com.lucianobrsts.repository.entity.UsuarioEntity;
 import br.com.lucianobrsts.uteis.Uteis;
@@ -14,17 +19,18 @@ public class PessoaRepository {
 
 	@Inject
 	PessoaEntity pessoaEntity;
- 
+
 	EntityManager entityManager;
- 
+
 	/***
 	 * MÉTODO RESPONSÁVEL POR SALVAR UMA NOVA PESSOA
+	 * 
 	 * @param pessoaModel
 	 */
-	public void salvarNovoRegistro(PessoaModel pessoaModel){
- 
-		entityManager =  Uteis.JpaEntityManager();
- 
+	public void salvarNovoRegistro(PessoaModel pessoaModel) {
+
+		entityManager = Uteis.JpaEntityManager();
+
 		pessoaEntity = new PessoaEntity();
 		pessoaEntity.setDataCadastro(LocalDateTime.now());
 		pessoaEntity.setEmail(pessoaModel.getEmail());
@@ -32,13 +38,65 @@ public class PessoaRepository {
 		pessoaEntity.setNome(pessoaModel.getNome());
 		pessoaEntity.setOrigemCadastro(pessoaModel.getOrigemCadastro());
 		pessoaEntity.setSexo(pessoaModel.getSexo());
- 
-		UsuarioEntity usuarioEntity = entityManager.find(UsuarioEntity.class, pessoaModel.getUsuarioModel().getCodigo()); 
- 
+
+		UsuarioEntity usuarioEntity = entityManager.find(UsuarioEntity.class,
+				pessoaModel.getUsuarioModel().getCodigo());
+
 		pessoaEntity.setUsuarioEntity(usuarioEntity);
- 
+
 		entityManager.persist(pessoaEntity);
- 
+
+	}
+
+	/***
+	 * MÉTODO PARA CONSULTAR A PESSOA
+	 * 
+	 * @return
+	 */
+	public List<PessoaModel> getPessoas() {
+
+		List<PessoaModel> pessoasModel = new ArrayList<PessoaModel>();
+
+		entityManager = Uteis.JpaEntityManager();
+
+		Query query = entityManager.createNamedQuery("PessoaEntity.findAll");
+
+		@SuppressWarnings("unchecked")
+		Collection<PessoaEntity> pessoasEntity = (Collection<PessoaEntity>) query.getResultList();
+
+		PessoaModel pessoaModel = null;
+
+		for (PessoaEntity pessoaEntity : pessoasEntity) {
+
+			pessoaModel = new PessoaModel();
+			pessoaModel.setCodigo(pessoaEntity.getCodigo());
+			pessoaModel.setDataCadastro(pessoaEntity.getDataCadastro());
+			pessoaModel.setEmail(pessoaEntity.getEmail());
+			pessoaModel.setEndereco(pessoaEntity.getEndereco());
+			pessoaModel.setNome(pessoaEntity.getNome());
+
+			if (pessoaEntity.getOrigemCadastro().equals("X"))
+				pessoaModel.setOrigemCadastro("XML");
+			else
+				pessoaModel.setOrigemCadastro("INPUT");
+
+			if (pessoaEntity.getSexo().equals("M"))
+				pessoaModel.setSexo("Masculino");
+			else
+				pessoaModel.setSexo("Feminino");
+
+			UsuarioEntity usuarioEntity = pessoaEntity.getUsuarioEntity();
+
+			UsuarioModel usuarioModel = new UsuarioModel();
+			usuarioModel.setUsuario(usuarioEntity.getUsuario());
+
+			pessoaModel.setUsuarioModel(usuarioModel);
+
+			pessoasModel.add(pessoaModel);
+		}
+
+		return pessoasModel;
+
 	}
 
 }
