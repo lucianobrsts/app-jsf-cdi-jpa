786bfb977eb04fe0c29dda2e37bc1caab35aee65
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-09-24 20:39:45 -0300

    Criada classe consultarPessoaCarouselController para nova consulta

---
+++ src/main/java/br/com/lucianobrsts/pessoa/controller/ConsultarPessoaCarouselController.java
@@ -0,0 +1,36 @@
+package br.com.lucianobrsts.pessoa.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Produces;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.lucianobrsts.model.PessoaModel;
+import br.com.lucianobrsts.repository.PessoaRepository;
+
+@Named(value = "consultaPessoaCarouselController")
+@ViewScoped
+public class ConsultarPessoaCarouselController implements Serializable {
+
+	private static final long serialVersionUID = 6206933676337240265L;
+
+	@Inject
+	transient private PessoaRepository pessoaRepository;
+
+	@Produces
+	private List<PessoaModel> pessoas;
+
+	public List<PessoaModel> getPessoas() {
+		return pessoas;
+	}
+
+	@PostConstruct
+	private void init() {
+		this.pessoas = pessoaRepository.getPessoas();
+	}
+
+}
