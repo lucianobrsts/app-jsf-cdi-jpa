68501080a93d4ca984749f80ffa36d73824a7be9
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-10-01 21:28:29 -0300

    Modificado formato

--- src/main/java/br/com/lucianobrsts/pessoa/controller/ConsultarPessoaCarouselController.java
+++ src/main/java/br/com/lucianobrsts/pessoa/controller/ConsultarPessoaCarouselController.java
@@ -2,7 +2,7 @@ package br.com.lucianobrsts.pessoa.controller;
 
 import java.io.Serializable;
 import java.util.List;
-
+ 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Produces;
 import javax.faces.view.ViewScoped;
@@ -11,26 +11,31 @@ import javax.inject.Named;
 
 import br.com.lucianobrsts.model.PessoaModel;
 import br.com.lucianobrsts.repository.PessoaRepository;
-
-@Named(value = "consultarPessoaCarouselController")
+ 
+ 
+@Named(value="consultarPessoaCarouselController")
 @ViewScoped
 public class ConsultarPessoaCarouselController implements Serializable {
-
-	private static final long serialVersionUID = 6206933676337240265L;
-
-	@Inject
-	transient private PessoaRepository pessoaRepository;
-
-	@Produces
+ 
+	private static final long serialVersionUID = 1L;
+ 
+	@Inject transient
+	private PessoaRepository pessoaRepository;
+ 
+	@Produces 
 	private List<PessoaModel> pessoas;
-
+ 
 	public List<PessoaModel> getPessoas() {
 		return pessoas;
 	}
-
+ 
 	@PostConstruct
-	private void init() {
+	private void init(){
+ 
 		this.pessoas = pessoaRepository.getPessoas();
 	}
-
+ 
+ 
+ 
+ 
 }
